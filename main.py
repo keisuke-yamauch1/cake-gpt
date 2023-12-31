86be358936aec6a425587cae67d2b88c9d091418
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2023-12-31 11:47:28 +0900

    feat: ディレクトリ移動に対応

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import logging
 import os
-import re
 
 from dotenv import load_dotenv
-from add_document import initialize_vectorstore
+
+from pinecone.add_document import initialize_vectorstore
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationalRetrievalChain
 
 logging.basicConfig(
     format="%(asctime)s [%(levelname)s] %(message)s", level=logging.INFO
@@ -16,28 +15,17 @@ if __name__ == "__main__":
     load_dotenv()
 
     vectorstore = initialize_vectorstore()
+
+    # OpenAIモデルの設定
     llm = ChatOpenAI(
-        model_name="gpt-3.5-turbo",
-        temperature=0,
-    )
-
-    condense_question_llm = ChatOpenAI(
         model_name=os.environ["OPENAI_API_MODEL"],
-        temperature=os.environ["OPENAI_API_TEMPERATURE"],
+        temperature=float(os.environ["OPENAI_API_TEMPERATURE"]),
     )
-
-    qa_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(),
-        condense_question_llm=condense_question_llm,
-    )
-
-    chat_history = ""
 
     # ユーザーからの入力を受け取る
     question = input("質問を入力してください: ")
 
-    # 辞書形式で質問とチャットの履歴を渡す
-    response = qa_chain.run({'question': question, 'chat_history': chat_history})
+    # OpenAIモデルを使用して質問に回答する（適切な入力形式を使用）
+    response = llm.invoke(question)
     print(response)
 
