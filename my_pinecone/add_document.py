3ebf667826ddb7112b8b14152b939607789f4ff3
Keisuke Yamauchi <keisuke.yamauchi.321@gmail.com>
2023-12-31 12:11:53 +0900

    feat: 構成変更

--- my_pinecone/add_document.py
+++ my_pinecone/add_document.py
@@ -1,13 +1,13 @@
-import json
 import logging
 import os
 
 import pinecone
+
 from dotenv import load_dotenv
 from langchain.document_loaders import TextLoader
-from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import Pinecone
+
+from init import initialize_vectorstore
 
 load_dotenv()
 
@@ -15,23 +15,6 @@ logging.basicConfig(
     format="%(asctime)s [%(levelname)s] %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
-
-
-def initialize_vectorstore():
-    pinecone.init(
-        api_key=os.environ["PINECONE_API_KEY"],
-        environment=os.environ["PINECONE_ENV"],
-    )
-
-    index_name = os.environ["PINECONE_INDEX"]
-
-    if index_name in pinecone.list_indexes():
-        pinecone.delete_index(index_name)
-
-    pinecone.create_index(name=index_name, metric="cosine", dimension=1536)
-
-    embeddings = OpenAIEmbeddings()
-    return Pinecone.from_existing_index(index_name, embeddings)
 
 
 def save_to_pinecone(file_path):
@@ -42,10 +25,20 @@ def save_to_pinecone(file_path):
 
     vectorstore = initialize_vectorstore()
     try:
+        init_document()
         vectorstore.add_documents(docs)
     except Exception as e:
         logger.error(f"Failed to add documents to Pinecone: {e}")
         return
+
+
+def init_document():
+    index_name = os.environ["PINECONE_INDEX"]
+
+    if index_name in pinecone.list_indexes():
+        pinecone.delete_index(index_name)
+
+    pinecone.create_index(name=index_name, metric="cosine", dimension=1536)
 
 
 if __name__ == "__main__":
